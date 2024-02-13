e9b2a3308b02f3b3660471496029aace7fe2e78e
Bob Webb <bob.wbb@gmail.com>
2024-02-13 17:26:15 +0100

    try to update the plugin implementation

--- src/index.ts
+++ src/index.ts
@@ -4,63 +4,75 @@ import {
   Context,
   Converter,
   Reflection,
+  ReflectionKind,
 } from 'typedoc';
-import * as ts from 'typescript';
 
 export function load(app: Application) {
   app.converter.on(
     Converter.EVENT_CREATE_DECLARATION,
-    (context: Context, reflection: Reflection, node: ts.Node) => {
-      injectSource(context, reflection, node);
+    (context: Context, reflection: Reflection) => {
+      injectSource(context, reflection);
     }
   );
   app.converter.on(
     Converter.EVENT_CREATE_SIGNATURE,
-    (context: Context, reflection: Reflection, node: ts.Node) => {
-      injectSource(context, reflection, node);
+    (context: Context, reflection: Reflection) => {
+      injectSource(context, reflection);
     }
   );
 }
 
-function injectSource(context: Context, reflection: Reflection, node: ts.Node) {
-  if (node) {
-    const sourceFile = node.getSourceFile();
-    if (reflection.comment?.tags) {
-      reflection.comment.tags = reflection.comment.tags.map((tag) => {
-        if (tag.tagName === 'source') {
-          return mapSourceTag(tag, node, sourceFile);
-        }
+function injectSource(context: Context, reflection: Reflection) {
+  // ignore project declaration
+  if(reflection.isProject()) return
+
+
+  if (reflection.comment?.blockTags) {
+    reflection.comment.blockTags = reflection.comment.blockTags.map((tag) => {
+      if (tag.tag === '@source') {
+
+        console.log(ReflectionKind[reflection.kind])
+        console.log(reflection.getFullName())
+
+        const sym = context.project.getSymbolFromReflection(reflection)
+
+        // console.log(sym?.valueDeclaration)
+        const vd = sym?.valueDeclaration
+        console.log(vd?.getText())
+        // let code: string | undefined = undefined
+        // if(reflection.isDeclaration()) {
+        //   const parent = sym?.valueDeclaration?.parent
+        //   if(!parent) return
+        //   code = parent.getText()
+        // } else {
+          // code = sym?.valueDeclaration?.getText()
+        // }
+
+
+        const code = ""
+
+        const newTag = mapSourceTag(tag, code);
+        return newTag
+      } else {
         return tag;
-      });
-    }
+      }
+    });
   }
 }
 
 function mapSourceTag(
   tag: CommentTag,
-  node: ts.Node,
-  sourceFile: ts.SourceFile
+  code: string
 ) {
-  const code = printNode(getNode(node), sourceFile);
-  return {
-    tagName: 'source',
-    paramName: '',
-    text: `${tag.text}\n\`\`\`typescript\n${stripBlockComments(
-      code
-    )}\n\`\`\`\n\n`,
-  };
-}
 
-function getNode(node: ts.Node) {
-  if (ts.isVariableDeclaration(node)) {
-    return node.parent;
-  }
-  return node;
-}
-
-function printNode(node: ts.Node, sourceFile: ts.SourceFile) {
-  const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
-  return printer.printNode(ts.EmitHint.Unspecified, node, sourceFile);
+  return new CommentTag("@source", [
+    {
+      kind: "code",
+      text: `${tag.content[0].text}\n\`\`\`typescript\n${stripBlockComments(
+        code
+      )}\n\`\`\`\n\n`
+    }
+  ])
 }
 
 function stripBlockComments(code: string) {
